dd40ef25b6a682eb33d92c7dd5c1141d91add9bf
Trevor Stanhope <trevor@W510>
2014-03-12 20:47:25 -0400

    bubble only for hum-temp

--- HiveAggregator.py
+++ HiveAggregator.py
@@ -4,9 +4,10 @@ HiveMind-Plus Aggregator
 Developed by Trevor Stanhope
 
 TODO:
-- Use MongoDB instead of CouchDB for storing
-- Implement graphing
+- Scatter plot, not line graph
+- convert to Flask?
 - Validate received data
+- POST to HiveServer
 """
 
 # Libraries
@@ -15,14 +16,18 @@ import json
 import ast
 import cherrypy
 import os
+import urllib2
+import sys
 from datetime import datetime, timedelta
 from pymongo import MongoClient
-from bson import json_util
 from cherrypy.process.plugins import Monitor
 from cherrypy import tools
 
 # Constants
-CONFIG_FILE = 'settings.json'
+try:
+    CONFIG_FILE = sys.argv[1]
+except Exception as err:
+    CONFIG_FILE = 'settings.json'
 
 # HiveAggregator
 class HiveAggregator:
@@ -33,7 +38,7 @@ class HiveAggregator:
         ### Load Configuration
         print('[Loading Config File]')
         with open(CONFIG_FILE) as config:
-            settings = ast.literal_eval(config.read())
+            settings = json.loads(config.read())
             for key in settings:
                 try:
                     getattr(self, key)
@@ -62,95 +67,88 @@ class HiveAggregator:
         print('[Enabling Monitors]')
         try:
             Monitor(cherrypy.engine, self.listen, frequency=self.CHERRYPY_LISTEN_INTERVAL).subscribe()
-            Monitor(cherrypy.engine, self.update, frequency=self.CHERRYPY_UPDATE_INTERVAL).subscribe()
         except Exception as error:
             print('--> ERROR: ' + str(error))
-    
-    ## Listen for Next Sample
-    def listen(self):
-        print('\n')
-
-        ### Receive Sample
+            
+    ## Receive Sample
+    def receive(self):
         print('[Receiving Sample from Hive]')
         try:
             packet = self.socket.recv()
             sample = json.loads(packet)
-            sample['time'] = datetime.now() # add datetime object for when sample was received
-            sample['date'] = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-            print('--> SAMPLE: ' + str(sample))
+            return sample
         except Exception as error:
             print('--> ERROR: ' + str(error))
-
-        ### Store to Mongo
+            
+    ## Check Sample
+    def check(self, sample):
+        sample['aggregator_id'] = self.AGGREGATOR_ID
+        return sample
+    
+    ## Post to Server
+    def post(self, sample):
+        print('[Posting Sample to Server]')
+        try:
+            data = json.dumps(sample)
+            req = urllib2.Request(self.POST_URL)
+            req.add_header('Content-Type','application/json')
+            response = urllib2.urlopen(req, data)
+            return response
+        except Exception as error:
+            print('--> ERROR: ' + str(error))
+            
+    ## Store to Mongo
+    def store(self, sample):
         print('[Storing to Mongo]')
         try:
+            sample['time'] = datetime.now()
             hive = self.mongo_db[sample['hive_id']]
             mongo_id = hive.insert(sample)
-            print('--> MONGO_SAMPLE_ID: ' + str(mongo_id))
+            return mongo_id
         except Exception as error:
             print('--> ERROR: ' + str(error))
-
-        ### Send Response
+    
+    ### Send Response
+    def send(self):
         print('[Sending Response to Hive]')
         try:
             response = {'status':'okay'}
             dump = json.dumps(response)
             self.socket.send(dump)
         except Exception as error:
-            print('--> ERROR: ' + str(error))
-
-    ## Update Hives
-    def update(self):
-        for estimator in ['temperature', 'humidity']:
-            ### Query Last 24 Hours
-            print('[Querying Last 24 Hours]')
-            hives = {}
-            sample_times = {} 
-            headers = ['date']
+            print('--> ERROR: ' + str(error))  
+               
+    ## Listen for Next Sample
+    def listen(self):
+        print('\n')
+        sample = self.receive() 
+        sample = self.check(sample)
+        response = self.post(sample)
+        mongo_id = self.store(sample)
+        self.send()
+        
+    ## Query Last 24 hours to CSV
+    def query_today(self):
+        print('[Querying Last 24 Hours]')
+        with open('data/data.csv', 'w') as datafile:
+            datafile.write('hive_id,time,temperature,humidity\n')
             one_day_ago = datetime.today() - timedelta(hours = 24) # get datetime of 1 day ago
-            print('--> RANGE: ' + str(one_day_ago))
-            for collection in self.mongo_db.collection_names():
-                if collection == 'system.indexes':
+            for name in self.mongo_db.collection_names():
+                if name == 'system.indexes':
                     pass
                 else:
-                    hive_samples = {}
-                    headers.append(collection) # build hive names
-                    hive = self.mongo_db[collection] # get hive
-                    for sample in hive.find({'time':{'$gt':one_day_ago}}):  
-                        sample['unix_time'] = datetime.strftime(sample['time'], '%s')
-                        sample['sample_time'] = datetime.strftime(sample['time'], '%Y-%m-%d-%H-%M')  
-                        sample_times[sample['unix_time']] = sample['sample_time'] # remember sample time
-                        hive_samples[sample['unix_time']] = sample # remember sample data
-                    hives[collection] = hive_samples
-
-            ### Flatten to List
-            print('[Flattening JSON to List]')
-            data_list = []
-            for unix_time in sample_times: # all times sampled
-                date_time = sample_times[unix_time] # i.e. sample['date']
-                data_point = [date_time]
-                for hive_id in hives:
-                    try:
-                        hive = hives[hive_id]
-                        matching_sample = hive[unix_time]
-                        if matching_sample['sample_time'] == sample_times[unix_time]:
-                            estimator_value = str(matching_sample[estimator])
-                            data_point.append(estimator_value)
-                    except Exception as err:
-                        pass # no time match
-                if len(data_point) == len(hives) + 1:
-                    data_list.append(data_point)
-            
-            ## Write to file
-            print('[Writing to CSV]')
-            with open('data/' + estimator + '.csv', 'w') as datafile:
-                datafile.write(','.join(headers) + '\n')
-                for data_point in sorted(data_list):
-                    datafile.write(','.join(data_point) + '\n')
+                    hive = self.mongo_db[name]
+                    for sample in hive.find({'time':{'$gt':one_day_ago}}):
+                        hive_id = str(sample['hive_id'])
+                        temperature = str(sample['temperature'])
+                        humidity = str(sample['humidity'])
+                        time = sample['time'].strftime('%H:%M')
+                        datafile.write(','.join([hive_id,time,temperature,humidity,'\n']))
     
     ## Render Index
     @cherrypy.expose
     def index(self):
+        self.query_today()                    
         html = open('static/index.html').read()
         return html
     
@@ -163,8 +161,5 @@ if __name__ == '__main__':
     conf = {
         '/': {'tools.staticdir.on':True, 'tools.staticdir.dir':os.path.join(currdir,'static')},
         '/data': {'tools.staticdir.on':True, 'tools.staticdir.dir':os.path.join(currdir,'data')}, # NEED the '/' before the folder name
-        'd3.v3.js': {'tools.staticfile.on': True, 'tools.staticfile.filename': os.path.join(currdir,'static') + 'd3.v3.js'},
-        'index.css': {'tools.staticfile.on': True, 'tools.staticfile.filename': os.path.join(currdir,'static') + 'index.css'},
-        '/favicon.ico': {'tools.staticfile.on': True, 'tools.staticfile.filename': os.path.join(currdir,'static') + 'favicon.ico'}
     }
     cherrypy.quickstart(aggregator, '/', config=conf)

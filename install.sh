976715f5914ca2f31771fd84f8504ec504c8c347
Trevor Stanhope <trevor@TS130>
2014-05-08 04:13:49 -0400

    diff debs

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 apt-get update -y
 apt-get upgrade -y
+apt-get install python-sklearn python-scipy python-numpy python-zmq
+apt-get install mongodb
 apt-get install pip -y
-pip install -r requirements.txt
+
+pip install pymongo

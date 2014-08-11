9577c04abdf65ec290c0e405773feac6a4f8aeef
Trevor Stanhope <trevor@localhost.localdomain>
2014-08-11 13:06:37 -0400

    added exec

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@ apt-get install python-pip -y
 apt-get install python-cherrypy3 -y
 pip install pymongo
 
-
+cp -R ../hive-aggregator /usr/share
+sudo ln -s /usr/share/hive-aggregator/bin/hive-aggregator /usr/bin/hive-aggregregator

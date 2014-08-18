859b75ffe3f1b9b34e95e6cd8c4b8cae07680d27
Evan Henry <evan@T530>
2014-08-18 14:59:12 -0400

    latest with estimators

--- install.sh
+++ install.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 # Get dependencies
-apt-get update -y
-apt-get upgrade -y
-apt-get install python-sklearn python-scipy python-numpy python-zmq -y
-apt-get install mongodb -y
-apt-get install python-pip -y
-apt-get install python-cherrypy3 -y
-pip install pymongo
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install python-sklearn python-scipy python-numpy python-zmq -y
+sudo apt-get install mongodb -y
+sudo apt-get install python-pip -y
+sudo apt-get install python-cherrypy3 -y
+sudo pip install pymongo
 
 # Install to path
-cp -R ../hive-aggregator /usr/share
-chmod +x /usr/share/hive-aggregator/configs/hive-aggregator
+echo "Install to /usr/bin/ [y/n]?"
+read ans
+if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
+then
+echo "installing to /usr/bin ..."
+sudo cp -R ../hive-aggregator /usr/share
+sudo chmod +x /usr/share/hive-aggregator/configs/hive-aggregator
 sudo ln -s /usr/share/hive-aggregator/configs/hive-aggregator /usr/bin/hive-aggregator
+fi
+if [ $ans = n -o $ans = N -o $ans = no -o $ans = No -o $ans = NO ]
+then
+echo "Skipping ..."
+fi

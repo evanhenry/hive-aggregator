9bd1cb0d73bd27e2419a4414beb61096f96df3f4
Trevor Stanhope <tpstanhope@gmail.com>
2015-02-06 17:54:55 -0500

    stuff

--- install.sh
+++ install.sh
@@ -10,16 +10,15 @@ sudo apt-get install python-cherrypy3 -y
 sudo pip install pymongo
 
 # Install to path
-echo "Install to /usr/bin/ [y/n]?"
-read ans
+read -p "Install to /usr/bin/ [y/n]?" ans
 if [ $ans = y -o $ans = Y -o $ans = yes -o $ans = Yes -o $ans = YES ]
-then
-echo "installing to /usr/bin ..."
-sudo cp -R ../hive-aggregator /usr/share
-sudo chmod +x /usr/share/hive-aggregator/configs/hive-aggregator
-sudo ln -s /usr/share/hive-aggregator/configs/hive-aggregator /usr/bin/hive-aggregator
+    then
+    echo "installing to /usr/bin ..."
+    sudo cp -R ../hive-aggregator /usr/share
+    sudo chmod +x /usr/share/hive-aggregator/configs/hive-aggregator
+    sudo ln -s /usr/share/hive-aggregator/configs/hive-aggregator /usr/bin/hive-aggregator
 fi
 if [ $ans = n -o $ans = N -o $ans = no -o $ans = No -o $ans = NO ]
-then
-echo "Skipping ..."
+    then
+    echo "Skipping ..."
 fi
